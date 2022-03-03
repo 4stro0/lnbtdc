2bd17f6b449b5928267abfe3e7a9eb339923b60e
naughtyboysd <100404622+naughtyboysd@users.noreply.github.com>
2022-03-03 11:20:35 +0800

    Added New Design

--- commands/Information/update.js
+++ commands/Information/update.js
@@ -10,7 +10,7 @@ module.exports = {
      */
     run: async(client , message , args) => {
         message.reply({embeds : [new MessageEmbed()
-        .setDescription('New Command `-helpmusic` , And Added The Music Command Check It Now! ')
+        .setDescription('**Patch Note 1.0**\n-Added Command Music\n-Added Slash Command\n-Added Argument In Help Command `-help ban`\n\n**Patch Note 2.0**(In Procces)\n-Redesigned Help Command\n-Lyrics Command\n-Added Button In some commands\n-Captcha System\n-Timeout Member\n**will be finished in 2 days**\nContributor: `naughtysd#4680`,`Yudd#5830`')
         .setColor(ee.color)
     ]})
     }
